9a64ff0451a01a8f8fe637aea906d2e59cf1b18c
Your Name <you@example.com>
2021-06-02 12:26:34 -0700

    Setting up THEN and CATCH with an API.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log(123);
+let url = 'https://catfact.ninja/breeds?limit=1';
+
+fetch(url)
+.then(response => response.json())
+.then(json => console.log(json))
+.catch(error => console.log('Error: ', error))
